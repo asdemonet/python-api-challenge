6b75acf0ccb193661d32367d221159e4aa1eac51
asdemonet <62965786+asdemonet@users.noreply.github.com>
2020-10-06 20:34:16 -0400

    change

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "81f7556fa919af3fe658b0c81b955549"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDHTsUjjjbWsaIk-pd2wkBpy73FAPiH_M0"
